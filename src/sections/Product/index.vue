dd4251f1b45014396803c52744d1446f14ab6541
KhaLe <minhkhanb@gmail.com>
2022-01-15 23:47:55 +0700

    pagination

---
+++ src/sections/Product/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="products max-w-screen-2xl mx-auto">
+    <ul :class="`grid grid-cols-${numOfColumn} gap-4 mb-4`">
+      <li :key="index" v-for="(product, index) in pageOfItems">
+        <div class="px-4 sm:px-6 py-2 sm:py-4 mx-auto bg-white shadow">
+          <div class="thumbnail h-64 mb-2">
+            <router-link to="#">
+              <img
+                class="rounded object-cover w-full h-full mx-auto"
+                :src="product.thumbnail"
+                alt=""
+              />
+            </router-link>
+          </div>
+          <div class="info">
+            <h3 class="font-bold text-gray-700 leading-snug text-xl">
+              <router-link to="#">{{ product.title }}</router-link>
+            </h3>
+            <div class="mt-2 text-sm text-gray-600">Liên hệ</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="pagination pb-4 pt-4">
+      <ul
+        v-if="pager.pages && pager.pages.length"
+        class="flex justify-center pl-0 list-none rounded my-2"
+      >
+        <li
+          :class="{ disabled: pager.currentPage === 1 }"
+          class="page-item first-item relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 ml-0 rounded-l hover:bg-gray-200"
+        >
+          <router-link :to="{ query: { page: 1 } }" class="page-link"
+            >First</router-link
+          >
+        </li>
+        <li
+          :class="{ disabled: pager.currentPage === 1 }"
+          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 hover:bg-gray-200"
+        >
+          <router-link
+            :to="{ query: { page: pager.currentPage - 1 } }"
+            class="page-link"
+            >Previous</router-link
+          >
+        </li>
+        <li
+          v-for="page in pager.pages"
+          :key="page"
+          :class="{ active: pager.currentPage === page }"
+          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 hover:bg-gray-200"
+        >
+          <router-link :to="{ query: { page: page } }" class="page-link">{{
+            page
+          }}</router-link>
+        </li>
+        <li
+          :class="{ disabled: pager.currentPage === pager.totalPages }"
+          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 border-r-0 hover:bg-gray-200"
+        >
+          <router-link
+            :to="{ query: { page: pager.currentPage + 1 } }"
+            class="page-link"
+            >Next</router-link
+          >
+        </li>
+        <li
+          :class="{ disabled: pager.currentPage === pager.totalPages }"
+          class="relative block py-2 px-3 leading-tight bg-white border border-gray-300 text-blue-700 rounded-r hover:bg-gray-200"
+        >
+          <router-link
+            :to="{ query: { page: pager.totalPages } }"
+            class="page-link"
+            >Last</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import paginate from 'jw-paginate';
+
+interface Page {
+  id: number;
+  title: string;
+  thumbnail: string;
+}
+
+interface Pager {
+  totalItems: number;
+  currentPage: number;
+  pageSize: number;
+  totalPages: number;
+  startPage: number;
+  endPage: number;
+  startIndex: number;
+  endIndex: number;
+  pages: number[];
+}
+
+export default defineComponent({
+  name: 'Product',
+  data() {
+    return {
+      numOfColumn: 4,
+      pager: {} as Pager,
+      pageOfItems: [] as Page[],
+    };
+  },
+  methods: {
+    onPageChanged() {
+      console.log('onPageChanged');
+    },
+    getProducts(curPage: number) {
+      const items = [...Array(160).keys()].map((i) => ({
+        id: i + 1,
+        title: 'Phôi cao su sẻ sấy ' + (i + 1),
+        thumbnail: 'http://gobencat.bizz.vn/img_products/4650/b1.jpg',
+      }));
+
+      // get page from query params or default to first page
+      const page = curPage || 1;
+
+      // get pager object for specified page
+      const pageSize = 12;
+      const pager = paginate(items.length, page, pageSize);
+
+      // get page of items from items array
+      const pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
+
+      // return pager object and current page of items
+      return { pager, pageOfItems };
+    },
+  },
+  watch: {
+    '$route.query.page': {
+      immediate: true,
+      handler(page: number) {
+        page = page || 1;
+        console.log('watch page: ', page, this.pager.currentPage);
+        if (page !== this.pager.currentPage) {
+          const { pager, pageOfItems } = this.getProducts(page);
+          console.log('pager: ', pageOfItems);
+          this.pager = pager;
+          this.pageOfItems = pageOfItems;
+        }
+      },
+    },
+  },
+});
+</script>
